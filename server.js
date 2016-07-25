739efe4a98f838417e59db0c811ab209bf2cab8c
Colin McDonald <mickydwow@gmail.com>
2016-07-24 22:29:31 -0700

    Added MongoDB

--- server.js
+++ server.js
@@ -7,16 +7,23 @@ const querystring = require('querystring');
 const publicPath = path.resolve(__dirname, 'public');
 const config = require('./config.js');
 const fetch = require('isomorphic-fetch');
-const cors = require('cors');
+const Promise = require('promise');
+const mongoose = require('mongoose');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const port = isProduction ? process.env.PORT : 3000;
+
+const Xray = require('x-ray');
+const x = Xray();
 
 const proxy = httpProxy.createProxyServer({
   changeOrigin: true
 });
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017')
+
+const db = mongoose.connection;
 
 app.use(express.static(publicPath));
 
@@ -24,7 +31,7 @@ const client_id = config.CLIENT_ID;
 const redirect_uri = config.REDIRECT_URI;
 const client_secret = config.SECRET;
 
-app.get('/login', function(req, res) {
+app.get('/login', (req, res) => {
   const scope = 'user-read-private user-read-email';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
@@ -36,7 +43,7 @@ app.get('/login', function(req, res) {
 
 });
 
-app.get('/callback', function(req, res) {
+app.get('/callback', (req, res) => {
   const code = req.query.code || null;
   fetch('https://accounts.spotify.com/api/token?' + querystring.stringify({
       code: code,
@@ -53,21 +60,30 @@ app.get('/callback', function(req, res) {
     .then(json => console.log(json))
 })
 
+app.post('/billboard', (req, res) => {
+  x('http://www.billboard.com/archive/charts/1991/hot-100', 'tbody', [{
+    song: ['.views-field-field-chart-item-song'],
+    artist: ['.views-field-field-chart-item-artist']
+  }])(function(err, title) {
+    console.log(title);
+  })
+})
+
 if (isProduction) {
   const bundle = require('./compiler/compiler.js')
   bundle()
-  app.all('/build/*', function (req, res) {
+  app.all('/build/*', (req, res) => {
     proxy.web(req, res, {
         target: 'http://localhost:8080'
     })
   })
 };
 
-proxy.on('error', function(e) {
-  console.log('Could not connect to proxy, please try again...')
-});
+proxy.on('error', (e) => console.log('Could not connect to proxy, please try again...'));
 
-app.listen(port, function () {
-  console.log('Server running on port ' + port)
-});
+app.listen(port, () => console.log('Server running on port ' + port));
 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected to mongodb')
+});
