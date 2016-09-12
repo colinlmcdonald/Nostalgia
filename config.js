b8d320d9571fe3bcfaee72c731643a59fa71c857
Colin McDonald <mickydwow@gmail.com>
2016-09-11 19:52:56 -0700

    Added config for heroku deploy

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  CLIENT_ID: '5c69223ff479449b8756b025b5429e1b',
+  SECRET: '0931e047571c44599f988e4f13720170',
+  REDIRECT_URI: 'http://localhost:3000/callback'
+}
---
+++ public/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  AUTH_TOKEN: '42dc80917bb1be82247201940eb57f8f',
+  SID: 'ACcec7f538b352c669cd9c4c1d8cecc4da'
+}
