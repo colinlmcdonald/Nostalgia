c75037d0ee115d1ed79e80b504d0f1fd472eb69d
Colin McDonald <mickydwow@gmail.com>
2016-09-28 01:08:57 -0700

    Finished styling song grid for now.

--- src/components/Track.js
+++ src/components/Track.js
@@ -5,19 +5,21 @@ import Spinner            from 'react-spinner';
 
 export const Track = ({track, i, handleSongPlaylist, handleSongPlay, currentSong, spotify}) => (
     <div className='song-container'>
-      <div className='song-name-album-container' onClick={() => handleSongPlaylist(track, i)}>
-        <p className='artist-song'>{track.song}</p>
-        <p className='artist'>{track.artist}</p>
-      </div>
       {track.album ? 
         <div className='album-img'>
           <img src={track.album.images[1].url} /> 
-            <PlusButton track={track} i={i} handleSongPlaylist={handleSongPlaylist} />
+          <PlayButton track={track} i={i} handleSongPlay={handleSongPlay} currentSong={currentSong}/>
         </div>
         : null}
+      <div className='song-name-album-buttons-container'>
+        <div className='song-name-album-container' onClick={() => handleSongPlaylist(track, i)}>
+          <p className='artist-song'>{track.song}</p>
+          <p className='artist'>{track.artist}</p>
+        </div> 
+        <div style={track.name ? {opacity: 1} : null} className='buttons-container'>
+          <PlusButton track={track} i={i} handleSongPlaylist={handleSongPlaylist} />
+        </div>
+        {!spotify ? <div className='spinner-container'><Spinner /></div> : null}
+      </div>
     </div>
 )
-      // <div style={track.name ? {width: 'auto', opacity: 1} : null} className='buttons-container'>
-      // <PlayButton track={track} i={i} handleSongPlay={handleSongPlay} currentSong={currentSong}/>
-      // </div> 
-      // {!spotify ? <div className='spinner-container'><Spinner /></div> : null}
